6feaa74794b3c9825b5854bc9fb76145456f02d4
Michael Berkowitz <michael.berkowitz@gmail.com>
2012-11-14 12:39:25 -0500

    Massively WIP overhaul.
    - Upgrade to rails3
    - Get server running, even if not serving anything really.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuestForLife::Application.config.secret_token = 'de238f2183114637370bfe93f12a4808a240ea220303dbf0327fa8a8aa15b523ecb9752a73e83ba3df03ddc257b6bef5e5533ea373c28a0542a32ad30e00f17d'
